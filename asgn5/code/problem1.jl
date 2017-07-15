f205ecc845c11c321c1892f7474f8b74b24d0a9f
Arg0n1s <basti_seppl@yahoo.com>
2017-07-15 12:32:23 +0200

    initial

---
+++ asgn5/code/problem1.jl
@@ -0,0 +1,29 @@
+using Images
+using PyPlot
+
+function fit_colors(img, fgmask, k)    
+
+end
+
+function data_term(img, fgm, bgm, s, t)
+end
+
+function contrast_weights(img, edges)
+end
+
+function make_edges(h,w)
+end
+
+function make_graph(h,w)
+end
+
+function smoothness_term(edges, lambda, W, hw)
+end
+
+function iterated_graphcut(img, bbox, lambda, k)
+end
+
+
+
+#imshow(iterated_graphcut(img, bbox, 10, 5);
+#title("Final Segmentation");
