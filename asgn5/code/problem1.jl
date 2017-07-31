47fa3e100032f4f466d07f867c3f8631caa5b4cf
Nicolas Acero <nicoacero94@gmail.com>
2017-07-31 22:29:53 +0200

    make_edges fertig

--- asgn5/code/problem1.jl
+++ asgn5/code/problem1.jl
@@ -1,7 +1,7 @@
 using Images
 using PyPlot
 
-function fit_colors(img, fgmask, k)    
+function fit_colors(img, fgmask, k)
 
 end
 
@@ -12,12 +12,30 @@ function contrast_weights(img, edges)
 end
 
 function make_edges(h,w)
+  edges = Vector{Array{Int64,2}}()
+  @assert h > 1 && w > 1
+  s = (h,w)
+  for j in 1:w
+    for i in 1:h
+      if j < w
+        push!(edges, [sub2ind(s,i,j) sub2ind(s,i,j+1)])
+      end
+      if i < h
+        push!(edges, [sub2ind(s,i,j) sub2ind(s,i+1,j)])
+      end
+    end
+  end
+  E = edges[1]
+  E = map(e -> E = vcat(E, e), edges[2:end])[end]
+  return E
 end
 
 function make_graph(h,w)
+  E = make_edges(h,w)
+
 end
 
-function smoothness_term(edges, lambda, W, hw)
+function smoothness_term(edges, W, lambda, hw)
 end
 
 function iterated_graphcut(img, bbox, lambda, k)
